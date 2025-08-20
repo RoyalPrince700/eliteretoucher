bf8067157d7bdc840f2f7531fd2c6c92d00b09d3
Royal Joseph <royalprincecube@gmail.com>
2025-08-20 17:49:09 +0100

    navbar updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,7 +41,7 @@ export const NavBar = () => {
       <header
         className={`fixed right-0 left-0 top-0 z-50 transition-all duration-300 
           border-b border-gray-300
-          ${isScrolled ? "bg-white/90 backdrop-blur-md shadow-sm" : "bg-transparent"}`}
+          ${isScrolled ? "bg-white/90 backdrop-blur-md shadow-sm" : "bg-gray-100"}`}
       >
         <div className="py-4">
           <div className="container mx-auto px-4">
